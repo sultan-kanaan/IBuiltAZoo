d7ac696f859fc182cb059fa18bbcd349efbf205e
Sultan Kanaan <98963963+sultan-kanaan@users.noreply.github.com>
2022-03-29 15:34:31 +0300

    Add project files.

---
+++ I built a Zoo!/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace I_built_a_Zoo_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
